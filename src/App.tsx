2df59db20cc8339576cec720925b2bdc7d4d0642
Davide Piccinini <davide.piccinini.95@gmail.com>
2025-05-24 21:39:03 +0200

    Fix card dimension

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ function App() {
 
         {/* RSVP Section */}
         <section id="rsvp" className="py-8 px-4 md:px-8">
-          <div className="max-w-xl mx-auto">
+          <div className="max-w-4xl mx-auto">
             <h2 className="text-3xl md:text-4xl text-center font-cursive text-sage-800 mb-12">
               <MessageCircle className="inline-block mr-2 mb-1" size={28} />
               {t("rsvp.title")}
